3c4485798f2b6c79f08ab83f994d377afe19a030
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2018-09-26 15:07:29 +0100

    Default to benchmark test

--- test.cpp
+++ test.cpp
@@ -67,6 +67,51 @@ int main()
 	delete[] mem;
 	return 0;
 }
+#elif PROF == 2
+int main()
+{
+        cudaError_t cudaStatus;
+        cudaStatus = cudaSetDevice(0); 
+        if (cudaStatus != cudaSuccess) {
+                fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+                return 1;
+        }
+        int N = 1<<22;
+#if TYPE==1
+        float* mem = new float[N];
+#elif TYPE ==2
+        double* mem = new double[N];
+#else   
+        unsigned int* mem = new unsigned int[N];
+#endif
+        for (int n = 0; n < N; n++)
+        {
+                mem[n] = rand();
+        }
+	
+	auto t1 = std::chrono::high_resolution_clock::now();
+        auto t2 = std::chrono::high_resolution_clock::now();
+        unsigned int count = 0;
+        while(std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() < 3000000)
+        {
+                cudaStatus = BitonicSort::BitonicSortCUDA(mem, N);
+                t2 = std::chrono::high_resolution_clock::now();
+                if (cudaStatus != cudaSuccess) {
+                        fprintf(stderr, "addWithCuda failed!");
+                        return 1;
+                }
+                count++;
+        }
+        // cudaDeviceReset must be called before exiting in order for profiling and
+        // tracing tools such as Nsight and Visual Profiler to show complete traces.
+        cudaStatus = cudaDeviceReset();
+        if (cudaStatus != cudaSuccess) {
+                fprintf(stderr, "cudaDeviceReset failed!");
+               return 1;
+        }
+        delete[] mem;
+        return 0;
+}
 #else
 int main()
 {
@@ -105,7 +150,7 @@ int main()
 		auto t2 = std::chrono::high_resolution_clock::now();
 		unsigned int count = 0;
 		cudaError_t cudaStatus;
-		while(std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() < 1000)
+		while(std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() < 30000)
 		{
 			cudaStatus = BitonicSort::BitonicSortCUDA(mem, N);
 			t2 = std::chrono::high_resolution_clock::now();
@@ -120,7 +165,7 @@ int main()
 		t1 = std::chrono::high_resolution_clock::now();
 		t2 = std::chrono::high_resolution_clock::now();
 		count = 0;
-		while (std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() < 5000)
+		while (std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() < 30000)
 		{
 			BitonicSort::BitonicSort(mem, N);
 			t2 = std::chrono::high_resolution_clock::now();
